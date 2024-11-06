022af3366e7631fd77a2bfb1723a5cf4e4e6f1a0
Sai Shanmukh Habbar Kalle <76609011+shanmukh06@users.noreply.github.com>
2024-11-06 12:03:05 +0530

    Update shanmukh06.c
    Created a calculator with addition and square function

--- shanmukh06.c
+++ shanmukh06.c
@@ -3,9 +3,14 @@ int addition(a, b){
   return a+b;
 }
 
+int square(a){
+  return pow(a, 2);
+}
+
 int main(){
   int a, b;
+  printf("Enter a number a and b: ")
   scanf("%d %d", &a, &b);
-  printf("Addition of Two Numbers is ");
-  addition(a, b);
+  printf("Addition of Two Numbers is ", addition(a, b));
+  printf("Square of a Number a: ", square(a))
 }
